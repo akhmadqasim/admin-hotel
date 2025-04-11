66b1ff379f31cda54076360fe5d485d377419f6c
Akhmad Qasim <akhmadqasim@gmail.com>
2025-04-12 01:01:12 +0800

    feat: update reservation schema and CRUD operations to include additional fields (#9)

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -8,37 +8,53 @@ import {prisma} from "@/helper/prisma";
 import * as v from "valibot";
 
 const reservationSchema = v.object({
-  memberId: v.string(),
-  date: v.date(),
-  price: v.number(),
+    memberId: v.string(),
+    beginDate: v.optional(v.date()),
+    endDate: v.optional(v.date()),
+    roomNumber: v.optional(v.string()),
+    mealCost: v.optional(v.number()),
+    laundryCost: v.optional(v.number()),
+    otherCost: v.optional(v.number()),
 });
 
 export const GET = async () => {
-  const reservations = await prisma.reservation.findMany();
+    const reservations = await prisma.reservation.findMany();
 
-  return NextResponse.json({
-    reservations
-  });
+    return NextResponse.json({
+        reservations
+    });
 }
 
 export const POST = async (req: NextRequest) => {
-  try {
-    const body = await req.json();
+    try {
+        const body = await req.json();
 
-    const data = v.parse(reservationSchema, {
-      memberId: body.memberId,
-      date: new Date(body.date),
-      price: body.price
-    });
+        const data = v.parse(reservationSchema, {
+            memberId: body.memberId,
+            ...(body.beginDate ? {beginDate: new Date(body.beginDate)} : {}),
+            ...(body.endDate ? {endDate: new Date(body.endDate)} : {}),
+            ...(body.roomNumber ? {roomNumber: body.roomNumber} : {}),
+            ...(body.mealCost ? {mealCost: body.mealCost} : {}),
+            ...(body.laundryCost ? {laundryCost: body.laundryCost} : {}),
+            ...(body.otherCost ? {otherCost: body.otherCost} : {}),
+        });
 
-    const reservation = await prisma.reservation.create({
-      data
-    });
+        const reservation = await prisma.reservation.create({
+            data: {
+                memberId: data.memberId,
+                beginDate: data.beginDate,
+                ...(data.endDate ? {endDate: data.endDate} : {}),
+                ...(data.roomNumber ? {roomNumber: data.roomNumber} : {}),
+                ...(data.mealCost ? {mealCost: data.mealCost} : {}),
+                ...(data.laundryCost ? {laundryCost: data.laundryCost} : {}),
+                ...(data.otherCost ? {otherCost: data.otherCost} : {})
+            }
+        });
 
-    return NextResponse.json({
-      reservation
-    });
-  } catch (e) {
-    return NextResponse.json({message: "Invalid data"}, {status: 400});
-  }
+        return NextResponse.json({
+            reservation
+        });
+    } catch (e) {
+        return NextResponse.json({message: "Invalid data"}, {status: 400});
+    }
 }
