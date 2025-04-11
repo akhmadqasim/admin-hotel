ab398db27be1f8a146290049d7aa30ba1fbbd6e9
Akhmad Qasim <akhmadqasim@gmail.com>
2025-04-12 00:58:15 +0800

    feat: update reservation schema and CRUD operations to include additional fields

--- src/app/api/reservations/[reservationId]/route.ts
+++ src/app/api/reservations/[reservationId]/route.ts
@@ -8,95 +8,111 @@ import {prisma} from "@/helper/prisma";
 import * as v from "valibot";
 
 const reservationSchema = v.object({
-  memberId: v.string(),
-  date: v.date(),
-  price: v.number(),
+    memberId: v.string(),
+    beginDate: v.optional(v.date()),
+    endDate: v.optional(v.date()),
+    roomNumber: v.optional(v.string()),
+    mealCost: v.optional(v.number()),
+    laundryCost: v.optional(v.number()),
+    otherCost: v.optional(v.number()),
 });
 
 const reservationSchemaPatch = v.object({
-  memberId: v.optional(v.string()),
-  date: v.optional(v.date()),
-  price: v.optional(v.number()),
+    memberId: v.optional(v.string()),
+    beginDate: v.optional(v.date()),
+    endDate: v.optional(v.date()),
+    roomNumber: v.optional(v.string()),
+    mealCost: v.optional(v.number()),
+    laundryCost: v.optional(v.number()),
+    otherCost: v.optional(v.number()),
 });
 
 export const GET = async (req: NextRequest, {params}: { params: Promise<{ reservationId: string }> }) => {
-  const {reservationId} = await params;
+    const {reservationId} = await params;
 
-  const reservation = await prisma.reservation.findUnique({
-    where: {
-      id: reservationId
+    const reservation = await prisma.reservation.findUnique({
+        where: {
+            id: reservationId
+        }
+    });
+
+    if (!reservation) {
+        return NextResponse.json({message: 'Reservation not found'}, {status: 404})
     }
-  });
 
-  if (!reservation) {
-    return NextResponse.json({message: 'Reservation not found'}, {status: 404})
-  }
-
-  return NextResponse.json({reservation})
+    return NextResponse.json({reservation})
 }
 
 export const DELETE = async (req: NextRequest, {params}: { params: Promise<{ reservationId: string }> }) => {
-  const {reservationId} = await params;
+    const {reservationId} = await params;
 
-  const result = await prisma.reservation.delete({
-    where: {
-      id: reservationId
+    const result = await prisma.reservation.delete({
+        where: {
+            id: reservationId
+        }
+    })
+
+    if (!result) {
+        return NextResponse.json({message: 'Reservation not found'}, {status: 404})
     }
-  })
 
-  if (!result) {
-    return NextResponse.json({message: 'Reservation not found'}, {status: 404})
-  }
-
-  return new NextResponse(null, {status: 204});
+    return new NextResponse(null, {status: 204});
 };
 
 export const PUT = async (req: NextRequest, {params}: { params: Promise<{ reservationId: string }> }) => {
-  try {
-    const {reservationId} = await params;
+    try {
+        const {reservationId} = await params;
 
-    const body = await req.json();
+        const body = await req.json();
 
-    const data = v.parse(reservationSchema, {
-      memberId: body.memberId,
-      date: new Date(body.date),
-      price: body.price
-    });
+        const data = v.parse(reservationSchema, {
+            memberId: body.memberId,
+            ...(body.beginDate ? {beginDate: new Date(body.beginDate)} : {}),
+            ...(body.endDate ? {endDate: new Date(body.endDate)} : {}),
+            ...(body.roomNumber ? {roomNumber: body.roomNumber} : {}),
+            ...(body.mealCost ? {mealCost: body.mealCost} : {}),
+            ...(body.laundryCost ? {laundryCost: body.laundryCost} : {}),
+            ...(body.otherCost ? {otherCost: body.otherCost} : {}),
+        });
 
-    const reservation = await prisma.reservation.update({
-      where: {
-        id: reservationId
-      },
-      data
-    });
+        const reservation = await prisma.reservation.update({
+            where: {
+                id: reservationId
+            },
+            data
+        });
 
-    return NextResponse.json({reservation});
-  } catch (e) {
-    return NextResponse.json({message: "Invalid data"}, {status: 400});
-  }
+        return NextResponse.json({reservation});
+    } catch (e) {
+        return NextResponse.json({message: "Invalid data"}, {status: 400});
+    }
 }
 
 export const PATCH = async (req: NextRequest, {params}: { params: Promise<{ reservationId: string }> }) => {
-  try {
-    const {reservationId} = await params;
+    try {
+        const {reservationId} = await params;
 
-    const body = await req.json();
+        const body = await req.json();
 
-    const data = v.parse(reservationSchemaPatch, {
-      memberId: body.memberId,
-      date: new Date(body.date),
-      price: body.price
-    });
+        const data = v.parse(reservationSchemaPatch, {
+            ...(body.memberId ? {memberId: body.memberId} : {}),
+            ...(body.beginDate ? {beginDate: new Date(body.beginDate)} : {}),
+            ...(body.endDate ? {endDate: new Date(body.endDate)} : {}),
+            ...(body.roomNumber ? {roomNumber: body.roomNumber} : {}),
+            ...(body.mealCost ? {mealCost: body.mealCost} : {}),
+            ...(body.laundryCost ? {laundryCost: body.laundryCost} : {}),
+            ...(body.otherCost ? {otherCost: body.otherCost} : {}),
+        });
 
-    const reservation = await prisma.reservation.update({
-      where: {
-        id: reservationId
-      },
-      data
-    });
+        const reservation = await prisma.reservation.update({
+            where: {
+                id: reservationId
+            },
+            data
+        });
 
-    return NextResponse.json({reservation});
-  } catch (e) {
-    return NextResponse.json({message: "Invalid data"}, {status: 400});
-  }
+        return NextResponse.json({reservation});
+    } catch (e) {
+        return NextResponse.json({message: "Invalid data"}, {status: 400});
+    }
 }
